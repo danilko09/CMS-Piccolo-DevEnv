32f0dbdc08eb747816e507a5d8fb72a7ae161b8e
danilko09 <kolesnikov.da0@gmail.com>
2018-09-01 13:10:44 +0400

    Перенос окружения из репозитория CMS-Piccolo
    Просто копипаста с коммита 4c73683592342c16b22585776428f640d255999b,
    никаких изменений.

---
+++ utils/testAsserts.php
@@ -0,0 +1,21 @@
+<?php
+
+function assertEquals($actual, $expected, $message = null){
+    if($actual !== $expected){
+        $usrmsg = $message != null ? $message."\n" : "";
+        $assmsg = "Expected: ".var_export($expected, true).", but was: ".var_export($actual, true);
+        throw new AssertionError($usrmsg.$assmsg);
+    }
+}
+
+function assertTrue($assertion, $message = null){
+    assertEquals($assertion, true, $message);    
+}
+
+function assertFalse($assertion, $message = null){
+    assertEquals($assertion, false, $message);    
+}
+
+function assertNull($actual, $message = null){
+    assertEquals($actual, null, $message);
+}
